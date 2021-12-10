c7c9631ece44078d9be4439d3f4ca0764c2b3787
LohasOT <oliverhoang93@gmail.com>
2021-12-10 15:06:50 -0800

    connected rotues to notes.html and index.html

--- server.js
+++ server.js
@@ -2,3 +2,12 @@ const express = require('express')
 const path = require('path')
 
 const app = express()
+
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.get('/notes', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'notes.html'))
+})
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'))
+})
